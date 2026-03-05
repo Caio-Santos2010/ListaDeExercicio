508dec283ca9e5f547d69950efd4ea275f25b3ca
Caio-Santos2010 <caio.santos.amaral@escola.pr.gov.br>
2026-03-05 14:18:57 +0000

    lista3.js

---
+++ lista3.js
@@ -0,0 +1,35 @@
+//Criar um programa para verificar se o número 12 é divisivel por 2 e por 3
+let num = 12;
+if (num % 2 === 0 && num % 3 === 0) {
+    console.log('Divisivel por 2 e por 3')
+} else {
+    console.log('Não é divisivel por 2 e por 3')
+}
+
+//Criar um programa para mostrar um desconto de 12% para produtos acima de R$800
+let preco = 800;
+if (preco >= 800) {
+    let desconto = preco * 0.88;
+    console.log(`O preço com desconto é ${desconto}`)
+} else {
+    console.log('não tem desconto')
+}
+
+//Criar um programa que mostre os números de 1 a 10 usando laço repetição
+for (let i =1; i <=10; i++) {
+    console.log(i)
+}
+
+//Criar um programa para mostrar apenas os números pares entre 1 e 20
+for (let i =10; i >=1; i--) {
+    console.log(i)
+}
+
+//Criar um programa para mostrar apenas os números impares entre 1 e 20
+
+//Criar um programa para mostrar a tabuada do 5
+//Criar um programa para mostrar os números de 1 a 7 usando while
+//Criar um programa para mostrar a tabuada do 3 usando while
+//Criar um programa para mostrar o fatorial de 5
+//Criar um programa para saber se o número 7 é primário
+//Criar um programa para calcular a potência de 2³
